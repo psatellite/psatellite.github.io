4d2e4b645555aad56d0f72a96439c983ef4930b3
sjthomas-pss <66434038+sjthomas-pss@users.noreply.github.com>
2022-02-02 14:41:37 -0500

    add more to sim framework

---
+++ SimulationFramework/VCHelp/pssfooter.js
@@ -0,0 +1,2 @@
+document.getElementById('pssfoot').innerHTML =
+'<hr><a href="http://www.psatellite.com"><img src="./Images/PSSLogo.jpg" style="margin-bottom:-15px;"></a><br><br><p>Copyright 2011 Princeton Satellite Systems, all rights reserved.<br>Return to <a href="./index.html">VisualCommander Help Home</a></p>';
